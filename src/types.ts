b1db9b1908e89b055e8038a5cf16eb64d67dc15c
Fedemartinnie <federicom.nieva@gmail.com>
2024-05-18 23:28:33 -0300

    merge rama fede

--- src/types.ts
+++ src/types.ts
@@ -23,7 +23,7 @@ export interface movie {
     title: string,
     subtitle: string[],
     plot: string,
-    genre: genre,// agregar --> genre[] //string[],
+    genre: genre[],//string[],
     releaseYear: string,
     duration: string,
     director: string[],
