bd87e9c9e40bc6e0f5734341833f421428b9fa14
jeremias mazzetti <jere.mazzetti@gmail.com>
2024-06-09 07:54:14 -0300

    rutas privadas testeadas correctamente.

--- src/routes/Movie.ts
+++ src/routes/Movie.ts
@@ -3,14 +3,10 @@ import authorizationMiddleware from '../utils/Middleware';
 
 const moviesController = require('../controllers/Movie')
 const router = express.Router();
-/*
-router.get('/', authorizationMiddleware, moviesController.moviesResult);
-router.get('/:id', authorizationMiddleware, moviesController.getMovie);
-router.put('/rating/:id', authorizationMiddleware, moviesController.rate);
-*/
 
-router.get('/', moviesController.moviesResult);
-router.get('/:id', moviesController.getMovie);
-router.put('/rating/:id', moviesController.rate);
+
+router.get('/results',authorizationMiddleware, moviesController.moviesResult);
+router.get('/:id',authorizationMiddleware, moviesController.getMovie);
+router.put('/rating/:id',authorizationMiddleware, moviesController.rate);
 
 export default router;
