d85afb523d159d7e5a3c5d8f7de66e85d23992d6
jeremias mazzetti <jere.mazzetti@gmail.com>
2024-06-08 21:39:06 -0300

    rutas de movie publicas

--- src/routes/Movie.ts
+++ src/routes/Movie.ts
@@ -3,14 +3,14 @@ import authorizationMiddleware from '../utils/Middleware';
 
 const moviesController = require('../controllers/Movie')
 const router = express.Router();
-
+/*
 router.get('/results', authorizationMiddleware, moviesController.moviesResult);
 router.get('/:id', authorizationMiddleware, moviesController.getMovie);
 router.put('/rating/:id', authorizationMiddleware, moviesController.rate);
+*/
 
-/*
 router.get('/results', moviesController.moviesResult);
 router.get('/:id', moviesController.getMovie);
-router.put('/rating/:id', moviesController.rate);*/
+router.put('/rating/:id', moviesController.rate);
 
 export default router;
